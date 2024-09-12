066d519dcdc182cc4fcd5965e92ec12c54ec4739
denlillamurmeln <rooba87@gmail.com>
2024-09-12 13:48:45 +0200

    började på instruktioner

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,12 @@ public class Main {
                 ca 3 msk curry
                 1 msk paprikapulver
                 5 dl vispgrädde
-                2 dl*/
+                2 dl creme fraiche
+                2 msk koncentrerad apelsinjuice
+                2 msk kinesisk soya
+                2 krm svartpeppar*/
+
+        System.out.println("Instruktioner");
+        /*1. Skär kyckling*/
     }
 }

be93de0ee73d916bd0170dc2052bfe07e7c82ffa
Iasmina Alkashash <iasminakash@hotmail.com>
2024-09-12 13:48:56 +0200

    I changed the name of the recipe and continued the ingridient list

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Chicken pasta recipe");
+        System.out.println("Esy tomato sauce pasta recipe");
         System.out.println("_______________________");
         System.out.println("The indridient you will need");
         System.out.println("~300 gramms of pasta");
-        System.out.println("");
+        System.out.println("~2 dl of totamo sauce");
+        System.out.println("~50 gramms of Parmesam cheese");
+        System.out.println("1 tpsp of salt");
+        System.out.println("_______________________");
     }
 }

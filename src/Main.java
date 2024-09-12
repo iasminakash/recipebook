9195f5f87149c697cd5e37fc261deeef39fa246d
Iasmina Alkashash <iasminakash@hotmail.com>
2024-09-12 14:00:39 +0200

    I continued the step description.

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,9 @@ public class Main {
         System.out.println("1. Boil some salty water");
         System.out.println("2. When the water starts to boil add the pasta");
         System.out.println("3. Cook pasta for around 5-7 minutes");
-        System.out.println("4. ");
+        System.out.println("4. Preheat the tomato sause and let in cook for about 2-3 minutes");
+        System.out.println("5. Seal the cooked pasta and add it to the tomato sauce.");
+        System.out.println("6. Cook the pasta in the sauce for about 5 minutes");
+        System.out.println("7. Top the pasta with some shreddered ");
     }
 }

7480ac4170e546d29efcfb628b69d8190f06832e
Iasmina Alkashash <iasminakash@hotmail.com>
2024-09-12 11:21:26 +0200

    Creating a name of ingredien list

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Recipe of pasta");
+
+        System.out.println("Ingredients");
     }
 }

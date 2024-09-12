90ebadba727802fcc97068f6e262baa938b9b4cc
denlillamurmeln <rooba87@gmail.com>
2024-09-12 13:45:17 +0200

    skrev nästan hela receptet

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("kyckling");
-        //hästen är röd
-        System.out.println("blubb");
+        System.out.println("Kycklinggryta med curry");
+        /*Ingredienser:
+        700 g Kycklingfilé
+                2 röda paprikor
+                2 gula lökar
+                2 msk smör, att steka i
+                ca 3 msk curry
+                1 msk paprikapulver
+                5 dl vispgrädde
+                2 dl*/
     }
 }

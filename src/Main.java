f79a176393bd0632999c33194ebe62240cae3d56
Iasmina Alkashash <iasminakash@hotmail.com>
2024-09-12 13:44:25 +0200

    I created a name of the recipe and started writing ingridient list

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Chicken pasta recipe");
+        System.out.println("_______________________");
+        System.out.println("The indridient you will need");
+        System.out.println("~300 gramms of pasta");
+        System.out.println("");
     }
 }

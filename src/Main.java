7387d1c60e9b782e9e3943cb85904c31ccd638ca
denlillamurmeln <rooba87@gmail.com>
2024-09-12 13:22:11 +0200

    vet inte

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("howre u?");
+        System.out.println("kyckling");
+        //hästen är röd
+        System.out.println("blubb");
     }
 }
